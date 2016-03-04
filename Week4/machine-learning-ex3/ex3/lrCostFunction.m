eff7aec8e7d1c18e9fb4a8ae368a3ae010dce5ca
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-03 21:08:05 -0500

    Logistic regression costfunction
    This cost function calculate cost J and gradient descent by using
    vector with regularization.

--- Week4/machine-learning-ex3/ex3/lrCostFunction.m
+++ Week4/machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,11 +35,10 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
+J = (1/m) * sum(-1 * y .* log(sigmoid(X * theta)) - (1-y) .* log(1-sigmoid(X*theta))) + (lambda/(2*m)) * theta(2:end)' * theta(2:end);
 
-
-
-
-
+grad(1) = (1/m) * X(:,1)' * (sigmoid(X*theta) - y);
+grad(2:end) = (1/m) * X(:,2:end)' * (sigmoid(X*theta) - y) + ((lambda/m) * theta(2:end));
 
 
 
