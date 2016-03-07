2a4b8a201c9ab5fc1035c1bc71bbd0c033cf9e62
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-07 13:55:12 -0500

    Add Optional exercise

--- Week5/machine-learning-ex4/ex4/ex4.m
+++ Week5/machine-learning-ex4/ex4/ex4.m
@@ -231,4 +231,37 @@ pred = predict(Theta1, Theta2, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 
+%% =============== Part 11: Optional exercise ===========
+
+for i = 1:10
+  options = optimset('MaxIter', 50);
+
+  %  You should also try different values of lambda
+  lambda = 0.5*i;
+
+  % Create "short hand" for the cost function to be minimized
+  costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+  % Now, costFunction is a function that takes in only one argument (the
+  % neural network parameters)
+  [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+  % Obtain Theta1 and Theta2 back from nn_params
+  Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                   hidden_layer_size, (input_layer_size + 1));
+
+  Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                   num_labels, (hidden_layer_size + 1));
+
+  %fprintf('Program paused. Press enter to continue.\n');
+
+  pred = predict(Theta1, Theta2, X);
+
+  fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+  
+end
+
 
