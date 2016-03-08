5687021ba49dc66cabff8ae8a3a9e26dd9fc33b9
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-08 14:07:00 -0500

    Neural Network related functions.
    Add nnCostFunciton, predict, comoputeNuericalGradient,
    randInitializeWeights and sigmoidGradient functions.

--- Exercises/HighFrequency/predict.m
+++ Exercises/HighFrequency/predict.m
@@ -5,36 +5,14 @@ function p = predict(Theta1, Theta2, X)
 
 % Useful values
 m = size(X, 1);
-n = size(X, 2);
 num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
-
-% Add ones to the X data matrix
-X = [ones(m, 1) X];
-
-hiden = X * Theta1';
-
-a2 = [ones(m, 1) sigmoid(hiden)];
-hiden2 = a2 * Theta2';
-
-[maxp, index] = max(hiden2, [], 2);
-
-p = index;
-
-
+h1 = sigmoid([ones(m, 1) X] * Theta1');
+h2 = sigmoid([ones(m, 1) h1] * Theta2');
+[dummy, p] = max(h2, [], 2);
 
 % =========================================================================
 
