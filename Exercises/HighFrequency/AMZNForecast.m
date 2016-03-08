ecfe27594036837093d80a57c7a2c4a92f7144f4
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-03-08 14:04:34 -0500

    Add Neural Network prediction.

--- Exercises/HighFrequency/AMZNForecast.m
+++ Exercises/HighFrequency/AMZNForecast.m
@@ -21,35 +21,40 @@
 
 clear ; close all; clc
 
-% Load Training Data
-fprintf('Loading and Visualizing Data ...\n')
 
-AMZN = csvread('AMZN.txt');
-index = AMZN(:,1); X = AMZN(:, 2:6); y = AMZN(:, 7) + 1;
-
-input_layer_size = size(AMZN,1);
-num_labels = 2;
-
-%% =========== Part 1: Loading and Visualizing Data =============
+%% =========== Part 1: Loading Amazon minute trading Data =============
 %  We start the exercise by first loading and visualizing the dataset. 
-%  You will be working with a dataset that contains handwritten digits.
-%
+%  You will be working with a dataset that contains Amazon minute trading data.
+%% Load Training Data
+fprintf('Loading Data ...\n')
+
+AMZN = csvread('AMZN.csv');
+AMZN = AMZN(2:end,2:end);
+X = AMZN(:, 1:6); y = AMZN(:, 8);
+
+num_labels = 3;
+%% Setup the parameters you will use for this exercise
+input_layer_size = size(X, 2);  % 6 input features
+hidden_layer_size = 4;         % 4 hidden units
+num_labels = 3;                 % 3 labels, from 1 to 3, resparately represent to down, unchange, up.   
+
+%% ================ Part 2: Training One-vs-All logistic regreesion and Predict for One-Vs-All ================
+%  After ...
 
 fprintf('\nTraining One-vs-All Logistic Regression...\n')
 
-lambda = 0.6;
+lambda = 0.1;
 [all_theta] = oneVsAll(X, y, num_labels, lambda);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-
-%% ================ Part 3: Predict for One-Vs-All ================
-%  After ...
 pred = predictOneVsAll(all_theta, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ================ Part 3: Training One-vs-All LR with lambda from 0 to 2 and Predict ================
+%  After ...
 fprintf('\mTraining One-vs-All LR with different lambda from 0 to 2, and put accuracy one by one \n')
 
 for j = 1:10
@@ -59,3 +64,81 @@ for j = 1:10
   fprintf('\nLambda is %f. Training Set Accuracy: %f\n', lambda, mean(double(pred == y)) * 100);
 end
 
+
+%% ================ Part 4: Initializing Pameters ================
+%  In this part of the exercise, you will be starting to implment a two
+%  layer neural network that classifies digits. You will start by
+%  implementing a function to initialize the weights of the neural network
+%  (randInitializeWeights.m)
+
+
+fprintf('\nInitializing Neural Network Parameters ...\n')
+
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+% Unroll parameters
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+
+%% =============== Part 7: Implement Backpropagation ===============
+%  Once your cost matches up with ours, you should proceed to implement the
+%  backpropagation algorithm for the neural network. You should add to the
+%  code you've written in nnCostFunction.m to return the partial
+%  derivatives of the parameters.
+%
+fprintf('\nChecking Backpropagation... \n');
+
+%  Check gradients by running checkNNGradients
+checkNNGradients;
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+
+
+%% =================== Part 8: Training NN ===================
+%  You have now implemented all the code necessary to train a neural 
+%  network. To train your neural network, we will now use "fmincg", which
+%  is a function which works similarly to "fminunc". Recall that these
+%  advanced optimizers are able to train our cost functions efficiently as
+%  long as we provide them with the gradient computations.
+%
+fprintf('\nTraining Neural Network... \n')
+
+%  After you have completed the assignment, change the MaxIter to a larger
+%  value to see how more training helps.
+options = optimset('MaxIter', 50);
+
+%  You should also try different values of lambda
+lambda = 0.1;
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
+%% ================= Part 10: Implement Predict =================
+%  After training the neural network, we would like to use it to predict
+%  the labels. You will now implement the "predict" function to use the
+%  neural network to predict the labels of the training set. This lets
+%  you compute the training set accuracy.
+
+pred = predict(Theta1, Theta2, X);
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
