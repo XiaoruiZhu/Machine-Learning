813ed5a55597c95466e161dd61b2aa5535e33d72
xiaoruizhu <zhuxiaorui1989@gmail.com>
2016-04-10 23:14:21 -0400

    Regularized learning function
    Contains X gradient descent and theta gradient descent.

--- Week9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Week9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -39,9 +39,12 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
+% J = 0.5 * sum(sum(((X * Theta' - Y) .^ 2) .* R));
 
+J = 0.5 * sum(sum(((X * Theta' - Y) .^ 2) .* R)) + lambda/2 * sum(sum(Theta .^ 2)) + lambda/2 *sum(sum(X .^ 2));
 
-
+X_grad =  (((X * Theta' - Y).* R) * Theta) + lambda .* X;
+Theta_grad =  (((X * Theta' - Y).* R)' * X) + lambda .* Theta;
 
 
 
